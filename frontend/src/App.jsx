16065a70c8754e55fd4ca2064b67e237bb551d5b
Muhammad-Farasat <farasatkhan687@gmail.com>
2025-04-30 11:43:38 +0500

    updated dist

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Signup from './pages/Signup'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Cookies from 'js-cookie'
-import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import CheckEmail from './pages/CheckEmail'
 import VerifyEmail from './pages/VerifyEmail'
@@ -17,28 +17,27 @@ function App() {
 
   return (
     <>
-
       <Routes>
 
-        {/* <Route path='/' element={authUser ? <Dashboard /> : <Navigate to={'/login'} />} /> */}
-        <Route path="/dashboard" element={<Dashboard />}>
+        <Route 
+          path="/dashboard" 
+          element={authUser ? <Dashboard /> : <Navigate to="/login" />}>
+
           <Route index element={<UsersTable />} />
           <Route path="users" element={<UsersTable />} />
           <Route path="edit-user/:id" element={<EditUser />} />
         </Route>
 
-
         <Route path='/signup' element={!authUser ? <Signup /> : <Navigate to={'/'} />} />
         <Route path='/login' element={!authUser ? <Login /> : <Navigate to={'/'} />} />
         <Route path="/check-email" element={<CheckEmail />} />
         <Route path="/verify-email/:token" element={<VerifyEmail />} />
+        
         <Route path="*" element={<Navigate to="/dashboard" />} />
 
       </Routes>
 
-      {/* <Signup /> */}
       <Toaster />
-
     </>
   )
 }
